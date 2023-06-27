1ec766ff413e60db6d26deff759f009fdbf61fff
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-06-27 17:13:01 +0100

    Transcriptions Displayed

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function IndexPage() {
         <Link
           target="_blank"
           rel="noreferrer"
-          href="/"
+          href="/transcription"
           className={buttonVariants({ variant: "secondary" })}
         > Login with GitHub
         </Link>
