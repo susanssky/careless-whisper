1c1d127abe02c44dee027527ebe76cd58e825826
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-05 03:31:19 +0100

    Sentences Component Updated!

--- components/Sentences.tsx
+++ components/Sentences.tsx
@@ -12,12 +12,9 @@ const Sentences = ({ sentences }: SentencesProps) => {
       {sentences.map((sentence) => (
         <div
           key={sentence.id}
-          className="flex p-3 gap-4 my-3 rounded-xl border-[1px] border-zinc-600 w-3/4"
+         
         >
-          <div className="flex flex-col gap-2">
-            <span className="text-xl font-semibold">{sentence.content}</span>
-            <span className="text-lg font-light">{sentence.id}</span>
-          </div>
+            {sentence.content}
         </div>
       ))}
     </>
