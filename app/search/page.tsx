c7d54c918c1c085ad8be4f74058be1609134f452
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-05 03:33:03 +0100

    Search Page Created!

---
+++ app/search/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useRouter, useSearchParams } from "next/navigation"
+import useSWR from "swr"
+
+import Sentences from "@/components/Sentences"
+
+import Spinner from "./Spinner"
+
+const fetchPosts = async (url: string) => {
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts")
+  }
+
+  return response.json()
+}
+
+const SearchPage = () => {
+  const search = useSearchParams()
+  const searchQuery = search ? search.get("q") : null
+  const router = useRouter()
+
+  const encodedSearchQuery = encodeURI(searchQuery || "")
+
+  const { data, isLoading } = useSWR(
+    `/api/search?q=${encodedSearchQuery}`,
+    fetchPosts,
+    { revalidateOnFocus: false }
+  )
+
+  if (!encodedSearchQuery) {
+    router.push("/")
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (!data?.length) {
+    return null
+  }
+
+  return (
+    <>
+      <span className="text-xl">
+        Showing results for:{" "}
+        <span className="font-semibold">{searchQuery}</span>
+      </span>
+      <Sentences sentences={data} />
+    </>
+  )
+}
+
+export default SearchPage
