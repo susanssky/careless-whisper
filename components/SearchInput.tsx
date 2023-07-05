fca3176e62719be06f52dd24afa543c41fe7ef06
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-05 08:07:34 +0100

    updated search bar style

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -22,12 +22,12 @@ const SearchInput = () => {
   }
 
   return (
-    <form onSubmit={onSearch} className="flex justify-center w-2/3">
+    <form onSubmit={onSearch} className="flex justify-center w-1/3 ml-5 my-5">
       <input
         value={searchQuery || ""}
         onChange={(event) => setSearchQuery(event.target.value)}
         className="px-5 py-1 w-2/3 sm:px-5 sm:py-3 flex-1 text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-green-700 placeholder:text-zinc-400"
-        placeholder="What are you looking for?"
+        placeholder="Search the text here..."
       />
     </form>
   )
