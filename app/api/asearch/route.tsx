76231e6747832e0ad3e84ee884cb29cdfb20a1d1
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-09 18:22:59 +0100

    exact match sorted

--- app/api/asearch/route.tsx
+++ app/api/asearch/route.tsx
@@ -1,52 +1,123 @@
 import { NextRequest, NextResponse } from "next/server"
-
+import { Sentence } from "@prisma/client"
 
 import { prisma } from "@/lib/prisma"
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const user = searchParams && searchParams.get("user")
+  const cohort = searchParams && searchParams.get("cohort")
   const keywords = searchParams && searchParams.get("keywords")
-  const category = searchParams && searchParams.get("category")
   const syllabusModule = searchParams && searchParams.get("syllabusModule")
   const duration = searchParams ? searchParams.get("duration") || "" : ""
 
   try {
- 
+    let sentences = []
 
-    const sentences = await prisma.sentence.findMany({
-      where: {
-       
-        transcription: {
-          post: {
-            user: {
-              name: {
-                contains: user || "",
-                mode: "insensitive",
+    if (user) {
+      const userSentences = await prisma.sentence.findMany({
+        where: {
+          transcription: {
+            post: {
+              user: {
+                name: {
+                  equals: user,
+                  mode: "insensitive",
+                },
               },
-            },
-            cohort: {
-              name: {
-                contains: category || "",
-                mode: "insensitive",
-              },
-            },
-            syllabus: {
-              name: {
-                contains: syllabusModule || "",
-                mode: "insensitive",
-              },
-            },
-            duration: {
-              equals: duration !== "" ? parseInt(duration) : undefined,
             },
           },
         },
-      },
-      include: {
-        transcription: true,
-      },
-    })
+        include: {
+          transcription: true,
+        },
+      })
+
+      sentences.push(...userSentences)
+    }
+
+    if (cohort) {
+      const cohortSentences = await prisma.sentence.findMany({
+        where: {
+          transcription: {
+            post: {
+              cohort: {
+                name: {
+                  equals: cohort,
+                  mode: "insensitive",
+                },
+              },
+            },
+          },
+        },
+        include: {
+          transcription: true,
+        },
+      })
+
+      sentences.push(...cohortSentences)
+    }
+
+    if (keywords) {
+      const keywordSentences = await prisma.sentence.findMany({
+        where: {
+          transcription: {
+            post: {
+              keywords: {
+                equals: keywords,
+                mode: "insensitive",
+              },
+            },
+          },
+        },
+        include: {
+          transcription: true,
+        },
+      })
+
+      sentences.push(...keywordSentences)
+    }
+
+    if (syllabusModule) {
+      const syllabusSentences = await prisma.sentence.findMany({
+        where: {
+          transcription: {
+            post: {
+              syllabus: {
+                name: {
+                  equals: syllabusModule,
+                  mode: "insensitive",
+                },
+              },
+            },
+          },
+        },
+        include: {
+          transcription: true,
+        },
+      })
+
+      sentences.push(...syllabusSentences)
+    }
+
+    if (duration) {
+      const durationSentences = await prisma.sentence.findMany({
+        where: {
+          transcription: {
+            post: {
+              duration: {
+                equals: parseInt(duration),
+              },
+            },
+          },
+        },
+        include: {
+          transcription: true,
+        },
+      })
+
+      sentences.push(...durationSentences)
+    }
 
     return NextResponse.json(sentences)
   } catch (error: any) {
