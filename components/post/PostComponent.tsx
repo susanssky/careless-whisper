6aaa274d9aa8f570c1cd0b4bcc9c09996fce6558
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-17 19:30:22 +0100

    client side

--- components/post/PostComponent.tsx
+++ components/post/PostComponent.tsx
@@ -8,6 +8,7 @@ import { ThickArrowUpIcon } from "@radix-ui/react-icons";
 
 
 import { deletePost } from "@/lib/deletePost";
+import { updatePost } from "@/lib/updatePost";
 import { voteTranscription } from "@/lib/voteTranscription";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Badge } from "@/components/ui/badge";
@@ -22,11 +23,9 @@ type PostComponentProps = {
   post: PostType
   session: any
 }
+
 export default function PostComponent({ post, session }: PostComponentProps) {
   const router = useRouter()
-
-
-
 
   const {
     id,
@@ -42,6 +41,7 @@ export default function PostComponent({ post, session }: PostComponentProps) {
     transcription,
     summary,
   } = post
+
   console.log(originalVideoLink)
 
   const article = transcription.sentences
@@ -79,7 +79,6 @@ const handleVote = async () => {
   }
 }
 
-
   const handleDelete = async (postId: number) => {
     const success = await deletePost(postId.toString())
 
@@ -91,44 +90,133 @@ const handleVote = async () => {
     }
   }
 
+  const [edit, setEdit] = useState(false)
+
+  const [postData, setPostData] = useState({
+    cohortName: post.cohort.name,
+    syllabusName: post.syllabus.name,
+    sessionName: post.sessionName || "",
+    leaderName: post.leaderName || "",
+    summary: post.summary || "",
+  })
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setPostData({ ...postData, [name]: value })
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try {
+      const success = await updatePost(post.id.toString(), postData)
+      if (success) {
+        setEdit(false)
+      } else {
+        setError("Failed to update post. Please try again.")
+      }
+    } catch (error) {
+      console.error("Failed to update post:", error)
+      setError("Failed to update post. Please try again.")
+    }
+  }
+
+  const handleEditClick = () => {
+    setEdit(true)
+  }
+
   if (!post) {
     return <div>Loading...</div>
   }
 
   return (
     <>
-      <div className="flex justify-center gap-4">
-        <Button asChild variant="destructive">
-          <Link href="/dashboard">Back to the dashboard</Link>
-        </Button>
-        {session?.user.role === "Mentor" && (
-          <>
-            <Button asChild>
-              <Link href="">Edit</Link>
+      {edit ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Cohort Name:
+            <input
+              type="text"
+              name="cohortName"
+              value={postData.cohortName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Syllabus Name:
+            <input
+              type="text"
+              name="syllabusName"
+              value={postData.syllabusName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Session Name:
+            <input
+              type="text"
+              name="sessionName"
+              value={postData.sessionName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Leader Name:
+            <input
+              type="text"
+              name="leaderName"
+              value={postData.leaderName}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Summary:
+            <textarea
+              name="summary"
+              value={postData.summary}
+              onChange={handleInputChange}
+            />
+          </label>
+          <Button type="submit">Save</Button>
+          <Button onClick={() => setEdit(false)}>Cancel</Button>
+        </form>
+      ) : (
+        <>
+          <div className="flex justify-center gap-4">
+             <Button asChild variant="destructive">
+            <Link href="/dashboard">Back to the dashboard</Link>
+          </Button>
+            {session?.user.role === "Mentor" && (
+              <>
+                <Button onClick={handleEditClick}>Edit</Button>
+                <Button onClick={() => handleDelete(id)}>Delete</Button>
+              </>
+            )}
+            <Button onClick={handleVote} disabled={voted}>
+              <ThickArrowUpIcon className="mr-2 h-4 w-4" />
+              {voted ? "Voted" : "Vote"}
             </Button>
-            <Button onClick={() => handleDelete(id)}>Delete</Button>
-          </>
-        )}
+          </div>
+          <Card>
+            <CardHeader>
+             <CardTitle>
+  {cohort.name}{" "}
+  {syllabus.link ? (
+    <Link href={syllabus.link} target="_blank">
+      {syllabus.name}
+    </Link>
+  ) : (
+    <span>{syllabus.name}</span>
+  )}
+  <br />
+  <p className="text-base">
+    {sessionName} by {leaderName}
+  </p>
+</CardTitle>
 
-         <Button onClick={handleVote} disabled={voted}>
-  <ThickArrowUpIcon className="mr-2 h-4 w-4" />
-  {voted ? "Voted" : "Vote"}
-</Button>
-
-      </div>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            {cohort.name}{" "}
-            <Link href={syllabus.link} target="_blank">
-              {syllabus.name}
-            </Link>
-            <br />
-            <p className="text-base">
-              {sessionName} by {leaderName}
-            </p>
-          </CardTitle>
-          <CardDescription>
+             <CardDescription>
             {originalVideoLink && (
               <>
                 Video:
@@ -141,32 +229,34 @@ const handleVote = async () => {
               <Badge>duration: {duration}min(s)</Badge>
               <Badge>views: {viewsNum}</Badge> <Badge>votes: {votesNum}</Badge>
             </div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Accordion
-            type="single"
-            defaultValue={summary ? "summary" : "transcription"}
-            collapsible
-          >
-            {summary && (
-              <AccordionItem value="summary">
-                <AccordionTrigger>Summary:</AccordionTrigger>
-                <AccordionContent>
-                  <p className="select-none">{summary}</p>
-                </AccordionContent>
-              </AccordionItem>
-            )}
-            <AccordionItem value="transcription">
-              <AccordionTrigger>Transcription:</AccordionTrigger>
-              <AccordionContent>
-                <p className="select-none">{article}</p>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </CardContent>
-        <CardFooter>Uploader: {user.name}</CardFooter>
-      </Card>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Accordion
+                type="single"
+                defaultValue={summary ? "summary" : "transcription"}
+                collapsible
+              >
+                {summary && (
+                  <AccordionItem value="summary">
+                    <AccordionTrigger>Summary:</AccordionTrigger>
+                    <AccordionContent>
+                      <p className="select-none">{summary}</p>
+                    </AccordionContent>
+                  </AccordionItem>
+                )}
+                <AccordionItem value="transcription">
+                  <AccordionTrigger>Transcription:</AccordionTrigger>
+                  <AccordionContent>
+                    <p className="select-none">{article}</p>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </CardContent>
+            <CardFooter>Uploader: {user.name}</CardFooter>
+          </Card>
+        </>
+      )}
     </>
   )
 }
